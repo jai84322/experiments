e36a65d26b1caab2fe521b25cd1459edc0869356
jai84322 <jai84322@gmail.com>
2022-05-14 19:08:28 +0530

    JSON-stringify

--- testing.js
+++ testing.js
@@ -68,12 +68,65 @@
 
 // -----------------------------------------
 
-let x = 7
-getName()
+// let x = 7
+// getName()
 
-function getName () {
-    console.log(x);
-}
+// function getName () {
+//     console.log(x);
+// }
 
-// ------------------------------------------
+// ------------------------- sum of array and product of array ----------
+// method-1 
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+// let sum=0;
+// for (i=0; i<arr.length; i++) {
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+// let product=1;
+// for (i=0; i<arr.length; i++) {
+//     product *= arr[i]
+// }
+// console.log(product);
+
+// // --------------------- simply print numbers from 1 to 10 -------------
+// let number;
+
+// for(i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// ------------- print function for odd number and even number
+// function findOdds (){
+// let array = [1,2,3,4,5,6,7,8,9]
+// let even= []
+// for ( i=0; i<array.length; i++) {
+//     if (array[i]%2 === 0){
+//         even.push(array[i])
+//     }
+// }
+// }
+ 
+
+// let arr = [1,2,3,4]
+
+// let myJSON = JSON.stringify(arr)
+
+// console.log(myJSON);
+// console.log(arr);
+
+// let person = {
+//     name : "jhon",
+//     age : 30,
+//     location : "jaipur"
+// }
+
+// let myJSON = JSON.stringify(person)
+// console.log(myJSON);
+
+// JSON parse converts a json file to object 
+// json stringify converts an object to string 
 
