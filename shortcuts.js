1ce6c303129da2816a25b697234be407bdc0bd0a
jai84322 <jai84322@gmail.com>
2022-06-11 11:36:55 +0530

    shortcuts

---
+++ shortcuts.js
@@ -0,0 +1,13 @@
+
+// SHORTCUTS AND CUSTOMER SHORTCUTS
+
+//ctrl+k = will SELECT till END of the line 
+//ctrl+h = will SELECT till HOME of the line 
+//ctrl+u = cursor HOME (till the home of the line) 
+//ctrl+i = cursor END (till the end of the line)
+//ctrl+shift+rightarrow = cursor word END select (word specific)
+//ctrl+shift+rightarrow = cursor word HOME select (word specific)
+//ctrl+leftarrow = cursor WORD END (word specific)
+//ctrl+rightarrow = cursor WORD HOME (word specific)
+//ctrl+j = full screen code only
+// ctrl+shift+c = full screen terminal only
