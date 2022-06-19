e3ed5b38e273eefda4c2ab64af5a00f042b39bcc
jai84322 <jai84322@gmail.com>
2022-06-19 18:51:01 +0530

    starting-some-concepts

---
+++ random.js
@@ -0,0 +1,55 @@
+// what is algo ? it is set of instructions written in the programming language 
+
+
+// let arr = [1,2,3,4,5]
+// let sum = 0
+// arr.forEach(arr => sum += arr )
+// console.log(sum);
+
+// let arr = [1,2,3,4,5]
+
+// function getIndex (arr,elem) {
+
+//     for (i=0;i<arr.length;i++) {
+//     if (arr[i] === elem) { 
+//         return i
+//     }
+// }
+//     return -1
+
+// }
+
+// console.log(getIndex([1,4,5,6,7,8,9,2,3,5],1))
+
+// function myGreet (n,m) {
+//     let k = 0
+//     for (i=1;i<n;i++) {
+//         for(j=1;j<n;j++) {
+//             for(l=1;l*l<m;l++) {
+//                 k++
+//             }
+//         }
+//     }
+//     console.log(k)
+// }
+
+// myGreet(6,5)
+
+
+
+/* 
+time complexity
+- array (searching is O(n)) and rest is constant
+- objects (searching is O(n)) and rest is constant 
+- map and set 
+- loops O(n) 
+- double loops O(n*m) 
+- triple loops O(n*n*under_root_m) 
+- 
+
+*/
+
+
+
+
+
