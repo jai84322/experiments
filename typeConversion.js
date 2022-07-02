8a2dbc97672ef1e206c2466f1d4cd820db8da489
jai84322 <jai84322@gmail.com>
2022-07-03 00:08:24 +0530

    typeconversion

---
+++ typeConversion.js
@@ -0,0 +1,128 @@
+
+// let obj = {
+//     fname: "jai",
+//     lname: "sharma",
+//     age: 30
+// }
+
+
+// let arr = JSON.stringify(obj);
+// console.log(arr);
+// console.log(typeof arr);
+// let jsObject = JSON.parse(arr)
+// console.log(typeof jsObject);
+
+// let arr = [ "alex", "ana", "florin" ]
+// console.log(arr);
+// console.log(typeof arr);
+
+// let newArr = JSON.stringify(arr)
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// let finalArr = JSON.parse(newArr)
+// console.log(finalArr);
+// console.log(typeof finalArr);
+
+// let str = "hello how are you doing today"
+// console.log(typeof str);
+
+// let newStr = Number(str)
+// console.log(typeof newStr);
+
+// let arr = "hello" // assigned string value
+// console.log(arr);
+// arr = 9 // reassigned number value
+// console.log(arr);
+
+// let y = "6"
+// let arr = 8
+// let g =7
+// let n = "9"
+
+// console.log(y+arr+g+n);
+
+// let a = "abc"
+// let b = 8
+// let c = String(true)
+// let d = "false"
+// let e = Boolean(undefined)
+// let f = null
+
+// console.log(c);
+// console.log(d);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof f);
+
+// let low = function hello () {
+//     console.log("hello");
+// }
+
+// low ()
+// let b = Boolean(low)
+
+// console.log(b);
+// console.log(typeof b);
+
+// let num = 123
+// let str = "456"
+
+// console.log(num === str);
+// console.log(typeof str);
+
+// console.log(num);
+
+// let num = Number("  ")
+// console.log(num)
+
+// let num = Number("44")
+// console.log(num + "56");
+
+// console.log(typeof "56");
+
+// console.log(Boolean("") + " = string quotes");
+// console.log(Boolean("  ") + " = only space in string");
+// console.log(Boolean(",") + " = comma ");
+// console.log(Boolean("-") + " = - in string");
+// console.log(Boolean("true") + " = true in string");
+// console.log(Boolean("false") + " = false in string");
+// console.log(Boolean("a") + " = character in string");
+// console.log(Boolean("-1") + " = negetive number in string");
+// console.log(Boolean("123") + " = number in string");
+// console.log(Boolean("null") + " = null in string");
+// console.log(Boolean("undefined") + " = undefined in string");
+// console.log(Boolean(0) + " = 0");
+// console.log(Boolean(-0) + " = -0");
+// console.log(Boolean(1) + " = number");
+// console.log(Boolean(null) + " = null");
+// console.log(Boolean(undefined) + " = undefined");
+// console.log(Boolean(NaN) + " = Nan");
+// console.log(Boolean(true) + " = boolean true");
+// console.log(Boolean(false) + " = boolean false");
+
+
+// false = string quotes
+// true = only space in string
+// true = comma
+// true = - in string
+// true = true in string
+// true = false in string
+// true = character in string
+// true = negetive number in string
+// true = number in string
+// true = null in string
+// true = undefined in string
+// false = 0
+// false = -0
+// true = number
+// false = null
+// false = undefined
+// false = Nan
+// true = boolean true
+// false = boolean false
+
+
