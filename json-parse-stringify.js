e2dd68ae2e83da70da4288d4702e90b7e6054d14
jai84322 <jai84322@gmail.com>
2022-07-04 04:32:47 +0530

    json-parse-stringify

---
+++ json-parse-stringify.js
@@ -0,0 +1,14 @@
+
+// let arr = '{"x":"5", "y":"r", "z":"u", "a":"6"}'
+
+// let newArr = JSON.parse(arr)
+// console.log(newArr);
+
+// let obj = {
+//     fname: "jai",
+//     lname : "sharma",
+//     age: 30
+// }
+
+// let newObj = JSON.stringify(obj)
+// console.log(newObj);
