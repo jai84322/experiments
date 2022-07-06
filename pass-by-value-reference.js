d9745b2e1f6681eb97c16d93a30ee2f2e6a9d50c
jai84322 <jai84322@gmail.com>
2022-07-06 09:45:45 +0530

    pass-by-value-reference

---
+++ pass-by-value-reference.js
@@ -0,0 +1,32 @@
+// let str = "hello"
+// let newStr = str
+// str = "mello"
+// console.log(str);
+// console.log(newStr);
+
+
+// let arr = [1,2,3,4]
+// let newArr = arr
+// arr[2] = 9
+// console.log(arr);
+// console.log(newArr);
+
+
+// let a = null
+// let b = null
+
+// console.log(a === b);
+
+// console.log(Boolean([0]));
+
+// below is pass by reference (only in non primitive cases)
+// value is not copied but the address of the value
+
+// const a = [1]
+// const b = a
+// console.log(a==b); // true
+
+// const a = [1]
+// const b = [1]
+// console.log(a==b); // false
+// becasue operator is only comparing with references here so false cauz a is refering to some block and b is also refering to some block 
