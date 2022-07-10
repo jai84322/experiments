2bbc2860ab0a54f2eb0971089552645634180b36
jai84322 <jai84322@gmail.com>
2022-07-11 00:49:29 +0530

    callbacks

---
+++ leaning.js
@@ -0,0 +1,28 @@
+// async function hello () {
+//     let h =  20*30 
+//     return h
+// }
+
+// let str = hello()
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// // console.log(hello());
+// console.log(str);
+
+
+// // async functions always returns a promise 
+
+// let aor = []
+
+// function hello () {
+//     console.log("hello");
+// }
+
+// function bye (a, callback) { 
+//     aor.push(a)
+//     callback()
+// }
+
+// bye({name: "jai sharma", age: 30}, hello)
+// console.log(aor);
